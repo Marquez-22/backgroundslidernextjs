62327c5a1d08a452ee089d2af3aaa9620df9f6b7
fatima <marquezlinares19@gmail.com>
2023-06-05 19:09:27 -0500

    reposicion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,8 @@ export default function Home() {
       "/images/img2.jpeg",
       "/images/img3.jpeg",
       "/images/img4.jpeg",
-      "/images/img5.jpeg",    
+      "/images/img5.jpeg",
+      "/images/img6.jpeg",    
     ]}
     
     
